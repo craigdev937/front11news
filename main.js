6f9f262ecdb61d6b1f737da0fb68bd9966aaa955
Craig Johnson <craigdev937@gmail.com>
2024-03-09 17:32:42 -0500

    Updated the header, navbar, menu, and hero.

--- main.js
+++ main.js
@@ -1,4 +1,34 @@
-console.log("Henry Quartermain!");
+"strict mode";
 
+const navList = document.querySelector(".navbar");
+const blurBg = document.querySelector(".nav__transparency");
+const menuBox = document.querySelector(".menu__btn");
+const menuIcon = document.querySelector(".menu__img");
+
+const resolution = () => {
+    if (window.innerWidth <= 544) {
+        menuBox.classList.remove("hidden");
+    } else if (menuBox && window.innerWidth > 544) {
+        menuBox.classList.add("hidden");
+        blurBg.classList.add("bg");
+        navList.classList.remove("nav-open");
+        menuIcon.src = `./icon-menu.svg`;
+    }
+};
+
+window.onload = resolution;
+window.onresize = resolution;
+
+const showMenu = () => {
+    blurBg.classList.toggle("nav");
+    navList.classList.toggle("nav-open");
+    menuIcon.src = `/icon-${
+        navList.classList.contains("nav-open") ? 
+        "menu-close" : 
+        "menu"
+    }.svg`;
+};
+
+menuBox.addEventListener("click", showMenu);
 
 
